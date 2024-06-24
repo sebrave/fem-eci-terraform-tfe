a341b2d78f52895d897757b7a5182b442d736ee5
Sebastian Ravenscroft <sebastian.ravenscroft@gmail.com>
2024-06-24 23:45:45 +0100

    feat: make github sync

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
-locals {
+remotes {
   project = {
     "fem-eci-project" = {
       description = "Example description"
@@ -13,7 +13,7 @@ locals {
     }
     "fem-eci-github" = {
       description         = "Example workspace for Github"
-      execution_mode      = "local"
+      execution_mode      = "remote"
       project_id          = module.project["fem-eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
